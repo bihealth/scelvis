cc842b016bfc69f4541a6ae281babe55f97d68b8
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-01-07 16:23:19 +0100

    Adding RTD workaround for git-lfs.

--- docs_manual/conf.py
+++ docs_manual/conf.py
@@ -7,11 +7,29 @@
 import os
 import sys
 
+
+# -- Workaround --------------------------------------------------------------
+#
+# Workaround to install and execute git-lfs on Read the Docs
+#
+# cf. https://github.com/readthedocs/readthedocs.org/issues/1846#issuecomment-477184259
+import os
+
+if not os.path.exists("./git-lfs"):
+    os.system(
+        "wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz"
+    )
+    os.system("tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz")
+    os.system("./git-lfs install")  # make lfs available in current repository
+    os.system("./git-lfs fetch")  # download content from remote
+    os.system("./git-lfs checkout")  # make local files to have the real content on them
+# ----------------------------------------------------------------------------
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
